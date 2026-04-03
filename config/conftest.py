4c40c2644d511c47b737ad86a7aa2a52abd0ca5c
David <david@example.com>
2026-04-03 23:57:50 +0800

    恢复--headless参数处理
    --headless是store_true类型，不需要传值

--- config/conftest.py
+++ config/conftest.py
@@ -28,7 +28,7 @@ def browser(request):
 @pytest.fixture(scope="session")
 def headless(request):
     """获取是否无头模式"""
-    return request.config.getoption("--headless") == "true"
+    return request.config.getoption("--headless")
 
 
 @pytest.fixture(scope="session")
